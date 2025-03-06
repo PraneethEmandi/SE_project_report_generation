cdd7547cedc8a2d2444705fc329f8125a9fa5485
EMANDI DEVI PRANEETH <praneethe4@gmail.com>
2025-03-06 13:19:01 +0530

    phase 1 final commit

--- graph.py
+++ graph.py
@@ -5,7 +5,13 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
+# from dotenv import load_dotenv
+# import os
 
+# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
+
+print('hj')
 # Database connection
 try:
     conn = mysql.connector.connect(
@@ -18,7 +24,7 @@ try:
 except mysql.connector.Error as err:
     print("Error: ", err)
     exit(1)
-
+print("hi")
 # Get user input for SQL query
 query = input("Enter your SQL query: ")
 cursor.execute(query)
